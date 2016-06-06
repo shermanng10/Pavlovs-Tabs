4bc3e7a6b809ee260e6091ab6ffa8c45317c6c24
shermanng10 <sherman.ng10@gmail.com>
2016-06-06 16:26:30 -0400

    Adds gif+overlay on top of newest tab

--- src/background.js
+++ src/background.js
@@ -1,8 +1,11 @@
+var newTabId;
+var randomGifUrl;
+
 function checkTabs(newTab){
 	chrome.tabs.query({currentWindow: true}, tabs => {
 		let tabCount = tabs.length;
 		let tabIds = [];
-		if (tabCount > 30) {
+		if (tabCount > 15) {
 			for (let x = 0; x < tabCount/2; x++){
 				tabIds.push(tabs[x].id)
 			}
@@ -12,7 +15,15 @@ function checkTabs(newTab){
 	});
 }
 
+function tabReadyListener(tabId, changeInfo, tab){
+	if (tabId == newTabId && changeInfo.status == 'complete') {
+		chrome.tabs.sendMessage(newTabId, { gifUrl: randomGifUrl });
+		chrome.tabs.onUpdated.removeListener(tabReadyListener);
+	};
+}
+
 function getGIF(newTab){
+	newTabId = newTab.id
 	let xhr = new XMLHttpRequest();
 	xhr.open("GET", "https://api.giphy.com/v1/gifs/search?q=bye&api_key=dc6zaTOxFJmzC");
 	xhr.send();
@@ -20,13 +31,13 @@ function getGIF(newTab){
 		if(xhr.readyState===4 && xhr.status===200){
 			let response = JSON.parse(xhr.response)
 			let data = response["data"]
-			let randomGifUrl = data[Math.floor(Math.random()*data.length)]["images"]["downsized_large"]["url"]
-			console.log(newTab.id)
-			chrome.tabs.sendMessage(newTab.id, {gifUrl: randomGifUrl})
-		}
+			randomGifUrl = data[Math.floor(Math.random()*data.length)]["images"]["downsized_large"]["url"]
+			chrome.tabs.onUpdated.addListener(tabReadyListener);
+    	}
 	}
 }
 
 chrome.tabs.onCreated.addListener((newTab) => {
 	checkTabs(newTab)
 });
+
